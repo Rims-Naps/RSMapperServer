d67dde3c01460d8bd013941814565e445480d068
Snow-RSPS <108329399+Rims-Naps@users.noreply.github.com>
2024-07-06 14:07:31 -0500

    Update IndexPacker.java

--- org/rsmapper/cache/scripts/IndexPacker.java
+++ org/rsmapper/cache/scripts/IndexPacker.java
@@ -8,8 +8,8 @@ package org.rsmapper.cache.scripts;
 import com.alex.store.Store;
 
 public class IndexPacker {
-    private static final Store matrix718 = new Store("C:/Users/Maria/Desktop/Runescape Private Servers/projects/Who Cares 667/ScapeSoft 667/data/cache/", true);
-    private static final Store ours = new Store("C:/Users/Maria/ScapeSoft_Cachev3.0/runescape/", false);
+    private static final Store matrix718 = new Store("C:/Users/smoke/Desktop/Runescape Private Servers/projects/Who Cares 667/ScapeSoft 667/data/cache/", true);
+    private static final Store ours = new Store("C:/Users/smoke/ScapeSoft_Cachev3.0/runescape/", false);
     private static final int INDEX = 8;
 
     public IndexPacker() {
